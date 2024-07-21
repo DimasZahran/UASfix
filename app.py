d3d59e4a019b31a911554f2eae7e15fc651f3624
DimasZahran <22611005@students.uii.ac.id>
2024-07-21 20:40:45 +0700

    app.py

--- app.py
+++ app.py
@@ -1,23 +1,9 @@
-from flask import Flask, request, jsonify
-import joblib
+import streamlit as st
 import numpy as np
+import pandas as pd
 
-app = Flask(__name__)
+st.title("Hello Streamlit!")
+st.write("This is a simple Streamlit app")
 
-# Load model terbaik
-model = joblib.load('best_model.pkl')
-
-@app.route('/')
-def home():
-    return "Model Deployment using Flask and Vercel.com"
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.get_json(force=True)
-    features = np.array(data['features']).reshape(1, -1)  # Mengubah input menjadi array 2D
-    prediction = model.predict(features)
-    output = prediction[0]
-    return jsonify({'prediction': output})
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+df = pd.DataFrame(np.random.randn(50, 20), columns=('col %d' % i for i in range(20)))
+st.dataframe(df)
