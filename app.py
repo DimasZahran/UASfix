1af666de43117370ab7a7a2aa0229898779aa875
DimasZahran <22611005@students.uii.ac.id>
2024-07-21 21:52:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,20 +14,22 @@ def load_model():
 
 # Muat model
 model = load_model()
-scaler = StandardScaler()
+scaler = StandardScaler()  # Anda harus memastikan scaler yang sama digunakan saat pelatihan
 
 if model:
-    st.title('Prediksi Status Transaksi')
+    st.title('Prediksi Status Transaksi UPI')
 
     # Input jumlah transaksi
     amount = st.number_input('Jumlah Transaksi (INR)', min_value=0.0, format='%f')
 
     if st.button('Prediksi'):
         try:
-            # Pra-pemrosesan dan prediksi
-            amount_scaled = scaler.fit_transform(np.array([[amount]]))
+            # Pra-pemrosesan input
+            amount_scaled = scaler.transform(np.array([[amount]]))
             prediction = model.predict(amount_scaled)
             status = 'Berhasil' if prediction[0] == 1 else 'Gagal'
             st.write('Prediksi Status:', status)
         except Exception as e:
             st.error(f"Terjadi kesalahan saat melakukan prediksi: {e}")
+else:
+    st.error("Model gagal dimuat.")
